ab140a31d9c7a983e7127cdc2da93012573c9ab2
Jairo Batista <jairo.batista21@gmail.com>
2019-12-01 14:54:53 -0400

    Restructured tests.

--- human_resources/tests/test_models.py
+++ human_resources/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import Employee
+from human_resources.models import Employee
 
 
 class EmployeeModelTests(TestCase):
