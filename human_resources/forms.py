bba30c4b157f218ca2e2715625fd1910930469a8
Jairo Batista <jairo.batista21@gmail.com>
2019-12-03 00:00:03 -0400

    Rendering date widget.

--- human_resources/forms.py
+++ human_resources/forms.py
@@ -1,8 +1,19 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from .models import Employee
+
+
+class DateInput(DateInput):
+    input_type = 'date'
 
 
 class EmployeeForm(ModelForm):
     class Meta:
         model = Employee
-        fields = ['forename', 'surname', 'middle_name', 'start_date', 'identification']
+        fields = ['forename', 'middle_name', 'surname', 'identification', 'start_date']
+        widgets = {
+            'start_date': DateInput,
+        }
+        labels = {
+            'forename': ('First Name'),
+            'surname': ('Last Name'),
+        }
