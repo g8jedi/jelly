b04779a06d112bca438bdcc292cbfdbe6c00ebd1
Jairo Batista <jairo.batista21@gmail.com>
2019-12-03 00:32:34 -0400

    Touch more test for Employee attributes.

--- human_resources/tests/test_models.py
+++ human_resources/tests/test_models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.test import TestCase
 from django.utils import timezone
 
@@ -12,19 +14,30 @@ class EmployeeModelTests(TestCase):
         forename = "Gianna"
         middle_name = "Haydee"
         surname = "Beato Batista"
+        hire_date = datetime.now()
         active = True
-        start_date = timezone.now()
+        date_of_birth = datetime.now()
+        email = "jairo@tacomoe.com"
+        phone_number = "8097553433"
+        nationality = "DOMINICAN"
+        gender = "FEMALE"
         identification = "230573204823"
         employee = Employee.objects.create(
-            forename=forename, middle_name=middle_name, surname=surname,
-            active=active, start_date=start_date, identification=identification
+            forename=forename, middle_name=middle_name, surname=surname, identification=identification,
+            hire_date=hire_date, date_of_birth=date_of_birth, active=active, email=email,
+            phone_number=phone_number, nationality=nationality, gender=gender
         )
 
         self.assertIs(employee.forename, forename)
         self.assertIs(employee.middle_name, middle_name)
         self.assertIs(employee.surname, surname)
-        self.assertIs(employee.active, active)
-        self.assertAlmostEquals(employee.start_date, start_date)
         self.assertIs(employee.identification, identification)
+        self.assertIs(employee.hire_date, hire_date)
+        self.assertIs(employee.date_of_birth, date_of_birth)
+        self.assertIs(employee.nationality, nationality)
+        self.assertIs(employee.active, active)
+        self.assertIs(employee.email, email)
+        self.assertIs(employee.phone_number, phone_number)
+        self.assertIs(employee.gender, gender)
 
-        #  2nd Iteration department, location, compesation: salary, hourly, etc...
+# 2nd Iteration department, location, compesation: salary, hourly, etc...
