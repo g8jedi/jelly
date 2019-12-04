4cbdc4a5c5da653f2f070ef040fd2b886e15a285
Jairo Batista <jairo.batista21@gmail.com>
2019-12-04 00:07:51 -0400

    Testing for employee detail view.

--- human_resources/tests/test_views.py
+++ human_resources/tests/test_views.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 from django.test import TestCase
 from django.urls import reverse
+
+from human_resources.models import Employee
 
 
 class EmployeeFormViewTests(TestCase):
@@ -15,3 +19,15 @@ class EmployeeFormViewTests(TestCase):
         self.assertContains(response, "Apellido")
         self.assertContains(response, "Identificacion")
         self.assertContains(response, "Sexo")
+
+
+class EmployeeDetailViewTests(TestCase):
+    def test_employee_detail_view_200(self):
+        employee = Employee.objects.create(
+            forename="Ana", middle_name="Mariela", surname="J", identification="341314", 
+            hire_date=datetime.today(), date_of_birth=datetime.today()
+        )
+
+        url = reverse('human_resources:employee-detail', args=(employee.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
